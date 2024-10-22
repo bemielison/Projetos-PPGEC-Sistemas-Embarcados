20333a873052704814b587229121a0004b875ca2
Bemielison Bezerra <bemielison@gmail.com>
2024-10-22 20:58:54 -0300

    Novos testes de interrupção
    Testes sem sucesso. Pelo que pesquisei há algo com a blackpill que não está gerando a interrupção.

--- Teste_EXTI_Interruption/Core/Src/main.c
+++ Teste_EXTI_Interruption/Core/Src/main.c
@@ -154,24 +154,27 @@ static void MX_GPIO_Init(void)
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if (GPIO_Pin == GPIO_PIN_13) {
+		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
+	} else {
+		__NOP();
+	}
+}
 /* USER CODE END 4 */
 
 /**
